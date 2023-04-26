159ad521ddf394559054ba7967cb9193d4cdc8b1
VIliusSenkus <vilius.s@outlook.com>
2023-04-26 14:03:28 +0300

    bandomas duomenų paėmimas iš backendo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,21 @@
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
 function App() {
+
+const [data, setData] = useState([]);
+
+
+useEffect(()=>{
+  axios.get('http://localhost:8000/api/prekes/')
+  .then(resp => setData(resp));
+}, []);
+
   return (
-    <div>Pagrindinis puslapis</div>
+    <div>
+      Pagrindinis puslapis
+      {data.map(d =><p>duomenys {d.pavadinimas} </p>)}
+    </div>
   );
 }
 
